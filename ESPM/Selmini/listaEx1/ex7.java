3a07b8bd63455e6ecbb025b0a1a0675cd27eb382
Fernanda Shinoda <63424056+InfiniteShadowz@users.noreply.github.com>
2020-04-21 11:30:56 -0300

    Create ex7.java

---
+++ ESPM/Selmini/listaEx1/ex7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class exercicio7 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		double a, b, c, d, x, y;
+		
+		//a)
+		y = ((2 + a) / ( b - 3)) - 2*x + Math.pow(x, 3);
+		
+		//b)
+		x = ((b / (a+c)) + 4*a) / ((d - 2*a) / 3+c);
+		
+		//c)
+		x = (-Math.pow(b, 15) - Math.sqrt(b*b - 4*a*c)) / ((d - 2*a) / (3+c));
+		
+		//d)
+		x = (Math.pow((b+1), 2) + c + a) / (b + 1/2 + (2*a) / (b*b));
+		
+		
+		
+
+	}
+
+}
