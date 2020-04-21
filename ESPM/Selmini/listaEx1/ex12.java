0bf7acc8f0ce41f801a97fce792e69c27373e60a
Fernanda Shinoda <63424056+InfiniteShadowz@users.noreply.github.com>
2020-04-21 12:14:51 -0300

    Create ex12.java

---
+++ ESPM/Selmini/listaEx1/ex12.java
@@ -0,0 +1,22 @@
+
+public class exercicio12 {
+
+	public static void main(String[] args) {
+		
+		double parafuPrec, porcaPrec, arruePrec,parafuPFinal, porcaPFinal, arruePFinal;
+		
+		parafuPrec = 0.3;
+		porcaPrec = 0.5;
+		arruePrec = 0.45;
+		
+		parafuPFinal = parafuPrec * 0.2;
+		porcaPFinal = porcaPrec * 0.1;
+		arruePFinal = arruePrec * 0.3;
+		
+		System.out.println(parafuPFinal);
+		System.out.println(porcaPFinal);
+		System.out.println(arruePFinal);
+
+	}
+
+}
