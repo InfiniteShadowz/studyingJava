fe670fe4ddd4b1c7417be9f2c0c09fbc7380a787
Fernanda Shinoda <63424056+InfiniteShadowz@users.noreply.github.com>
2020-04-21 12:03:39 -0300

    Create ex11.java

---
+++ ESPM/Selmini/listaEx1/ex11.java
@@ -0,0 +1,18 @@
+
+public class exercicio11 {
+
+	public static void main(String[] args) {
+		
+		double prodSemana, prodMes;
+		double quedaEnergia;
+		
+		prodSemana = 10000;
+		quedaEnergia = 0.05;
+		
+		prodMes = prodSemana*4 * 2*quedaEnergia;
+		
+		System.out.println(prodMes);
+		
+	}
+
+}
